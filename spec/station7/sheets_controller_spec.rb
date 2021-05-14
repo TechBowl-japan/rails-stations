a5ecc59017ff6aec6e4256e6a9b644d0a451e6c7
sohey-dr <heyso1209@gmail.com>
2021-05-14 19:35:52 +0900

    Add station7 tests

---
+++ spec/station7/sheets_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe SheetsController, type: :controller do
+  render_views
+  describe 'Station7 GET /sheets' do
+    let!(:sheets) { create_list(:sheet, 5) } 
+    before do
+      get 'index'
+    end
+
+    it '200を返すこと' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'HTMLの中にはtableタグがあること' do
+      expect(response.body).to include('</table>')
+    end
+
+    it '実装の中でsheetsテーブルにアクセスしていること' do
+      expect(controller.instance_variable_get("@sheets")).to eq sheets
+    end
+  end
+end
