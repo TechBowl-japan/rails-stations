b59ce803c88b2b6d5146a3aa592c619edf3499fa
sohey-dr <heyso1209@gmail.com>
2021-05-15 00:25:05 +0900

    Add staion6 movies index when search

--- spec/station6/movies_controller_spec.rb
+++ spec/station6/movies_controller_spec.rb
@@ -20,5 +20,22 @@ RSpec.describe MoviesController, type: :controller do
     it 'HTMLの中にはmoviesテーブルのレコード数と同じ件数のデータがあること' do
       expect(response.body).to include(movies[0].name).and include(movies[1].name).and include(movies[2].name).and include(movies[0].image_url).and include(movies[1].image_url).and include(movies[2].image_url)
     end
+
+    context '検索時' do
+      # factoriesのis_showingのデフォルトでは1
+      let(:show_estimated) { create(:movie, is_showing: 0 ) }
+      let(:showed_movie) { create(:movie, is_showing: 1 ) }
+  
+      it '検索キーワードを指定するとそれを含むものだけ表示' do
+        get :index, params: { name: show_estimated.name, is_showing: "" }
+        expect(response.body).to include(show_estimated.name)
+        expect(response.body).to not_include(showed_movie.name)
+      end
+
+      it '公開中か公開前の切り替えができる' do
+        get :index, params: { name: "", is_showing: 1 }
+        expect(response.body).to include("公開予定")
+      end
+    end
   end
 end
