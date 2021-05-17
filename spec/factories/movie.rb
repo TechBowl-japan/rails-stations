3dec9e917936df5e6c9bf84faf3f565c816f65cd
sohey-dr <heyso1209@gmail.com>
2021-05-17 18:50:43 +0900

    Delete description's indention

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :movie do
     sequence(:name) { |n| "TEST_MOVIE#{n}"}
     sequence(:year) { 2021 }
-    sequence(:description) { "この映画は最高です。\r\n改行しました"}
+    sequence(:description) { "この映画は最高です。改行しました"}
     sequence(:image_url) { |n| "https://techbowl.co.jp/_nuxt/img/#{n}.png" }
     sequence(:is_showing) { 1 }
   end
