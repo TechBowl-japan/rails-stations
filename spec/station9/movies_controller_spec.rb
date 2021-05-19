906235f7b07ba351ed3a98385c0f7e2b00b63ec2
sohey-dr <heyso1209@gmail.com>
2021-05-19 16:15:54 +0900

    Add station9 movies controller test

---
+++ spec/station9/movies_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+RSpec::Matchers.define_negated_matcher :not_include, :include
+
+RSpec.describe MoviesController, type: :controller do
+  render_views
+  describe 'Station6 GET /admin/movies' do
+    let!(:movies) { create_list(:movie, 3) }
+    before do
+      @schedules = create_list(:schedule, 3, movie_id: movies[0].id)
+      get 'index'
+    end
+
+    it 'movies(:id)に対応するscheduleを表示していること' do
+      expect(response.body).to include(@schedules[0].start_time.to_s).and include(@schedules[2].start_time.to_s)
+    end
+
+    # 以下はStation2のテストも通ることを確認
+
+    it '200を返すこと' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'HTMLを返すこと' do
+      expect(response.body).to include('<!DOCTYPE html>')
+    end
+
+    it 'HTMLの中にはtableタグがあること' do
+      expect(response.body).to include('<table>')
+    end
+
+    it 'HTMLの中にはmoviesテーブルの件数と同じだけのデータがあること' do
+      expect(response.body).to include(movies[0].name).and include(movies[1].name).and include(movies[2].name).and include(movies[0].image_url).and include(movies[1].image_url).and include(movies[2].image_url)
+    end
+
+    it 'HTMLの中にはtrue/falseが含まれないこと' do
+      expect(response.body).to not_include('true').and not_include('false')
+    end
+
+    describe 'HTMLの中にはmoviesテーブルのカラムすべてが表示されていること' do
+      it 'moviesテーブルのname,year,descriptionカラムが表示されていること' do
+        expect(response.body).to include(movies.first.name).and include(movies.first.year).and include(movies.first.description)
+      end
+
+      it 'moviesテーブル内のimage_urlが画像として表示されていること' do
+        expect(response.body).to include("<img src=\"#{movies.first.image_url}\"")
+      end
+    end
+  end
+end
