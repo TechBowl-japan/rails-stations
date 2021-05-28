d86b76046af5b9d51df7bbd2e7eebc1232fb63c5
sohey-dr <heyso1209@gmail.com>
2021-05-29 01:43:04 +0900

    Add Station13 test

---
+++ spec/station13/reservations_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ReservationsController, type: :controller do
+  describe 'Station13 POST /admin/reservations/' do
+
+    it '500が返らない' do
+    end
+  end
+end
