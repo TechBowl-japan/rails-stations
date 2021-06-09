7c2ec6e027396baacddf2ef7c168fd907134931d
sohey-dr <heyso1209@gmail.com>
2021-06-09 17:16:59 +0900

    Add comment

--- init/001_ddl.sql
+++ init/001_ddl.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `app_test`;
 CREATE DATABASE IF NOT EXISTS `app_development`;
-
+--  Docker buildで環境を全て揃えたいため、ここで必要なテーブルを作成
 CREATE TABLE IF NOT EXISTS `app_development`.`movies` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(160) NOT NULL COMMENT '映画のタイトル。邦題・洋題は一旦考えなくてOK',
