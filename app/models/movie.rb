58fa9b5751b55191dfb9aa58db8b778f597f42b1
neco3coffee <neco3coffee@gmail.com>
2021-08-22 08:04:14 +0000

    Fix movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  validates :name,  presence: true, uniqueness: true
+  validates :name,  uniqueness: true
   validates :image_url, length: { maximum: 50 }
 
   # validates :email, presence: true, length: { maximum: 255 },
