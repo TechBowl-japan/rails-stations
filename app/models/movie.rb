b6128cdf5d9ef09d3989439bf66a2cc971dca224
neco3coffee <neco3coffee@gmail.com>
2021-08-22 01:29:54 +0000

    Add: movie model validationãfor image_url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   validates :name,  presence: true, uniqueness: true
+  validates :image_url, length: { maximum: 50 }
 
   # validates :email, presence: true, length: { maximum: 255 },
   #           uniqueness: true
