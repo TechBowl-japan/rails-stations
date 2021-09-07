35eaacfcf7bc46beee19f054b9cbd9237853bf55
masahito-nagayama <me18sf67m@gmail.com>
2021-09-07 19:03:07 +0900

    fix migration file

--- db/migrate/20210907094945_create_movies.rb
+++ db/migrate/20210907094945_create_movies.rb
@@ -2,6 +2,7 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string "name"
+      t.string "image"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_094945) do
 
   create_table "movies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
