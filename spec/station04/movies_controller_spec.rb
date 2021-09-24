153dc391a719a8c74001e3a2bb54b3408e679c58
soheyHashimoto <heyso1209@gmail.com>
2021-09-24 13:00:04 +0900

    [DELETE]必要のないテストの削除

--- spec/station04/movies_controller_spec.rb
+++ spec/station04/movies_controller_spec.rb
@@ -37,11 +37,5 @@ RSpec.describe Admin::MoviesController, type: :controller do
       post :update, params: { id: movie.id, image_url: "https://techbowl.co.jp/_nuxt/img/111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111lllllllllllll.png" }, session: {}
       expect(response).to have_http_status(:ok)
     end
-
-    it 'DBに保存されていること' do
-      expect do
-        post :update, params: { id: movie.id, movie: movie_attributes }, session: {}
-      end.to change(Movie, :count).by(1)
-    end
   end
 end
