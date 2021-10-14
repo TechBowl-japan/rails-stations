0990d2a8cd09e5490c61c2bb62caa9f5558386f0
sum16 <godasper@gmail.com>
2021-10-14 16:49:55 +0900

    station3

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -19,12 +19,13 @@ class Admin::MoviesController < ApplicationController
   end
 
   # def create
+  #   @movie = Movie.new(movie_params)
   #   begin
-  #     Movie.create(movie_params)
-  #       redirect_to admin_movies_path, notice: "投稿しました"
+  #     @movie.save
+  #     redirect_to admin_movies_path, notice: "投稿しました"
   #   rescue ActiveRecord::ValueTooLong => e
   #     logger.error("エラー内容: #{e.message}")
-  #     flash.now[:alert] = "入力内容が間違っています"
+  #     render "errors/not_found", status: 404
   #     render :new
   #   end
   # end
