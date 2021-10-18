de308710c84b17f359ef321120bef6d2c05f2e62
sum16 <godasper@gmail.com>
2021-10-18 16:32:39 +0900

    staion4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActionController::RoutingError, with: :rescue404
   rescue_from Exception, with: :rescue500
-  rescue_from ActionController::BadRequest, with: :rescue400
-  rescue_from ActionController::ParameterMissing, with: :rescue400
+  # rescue_from ActionController::BadRequest, with: :rescue400
+  # rescue_from ActionController::ParameterMissing, with: :rescue400
 
 
   private
-  
-  def rescue400(e)
-    render "errors/bad_request", status: 400
-  end
+
+  # def rescue400(e)
+  #   render "errors/bad_request", status: 400
+  # end
 
   def rescue404(e)
     render "errors/not_found", status: 404
