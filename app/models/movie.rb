fe4cca782208729fb1e540a56328145d9ed84a4b
sum16 <godasper@gmail.com>
2021-10-24 04:27:48 +0900

    station6

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,15 +9,8 @@ class Movie < ApplicationRecord
     validates :name
   end
 
-  attr_accessor :flg
-
-  def self.set_init
-  
-  end
-
   scope :search, -> (movie_search_params) do
     return if movie_search_params.blank?
-    # search_paramsが空の場合以降の処理を行わない
 
     # パラメータを指定して検索を実行
     name_like(movie_search_params).filter_is_showing(movie_search_params)
@@ -27,6 +20,3 @@ class Movie < ApplicationRecord
   # is_showingが存在する場合、true or falseを返す
   scope :filter_is_showing, -> (movie_search_params) { where(is_showing: movie_search_params[:is_showing]) if movie_search_params[:is_showing].present? }
 end
-
-# Scopeとは、ActiveRecordのQueryメソッドに名前を付ける機能
-# 戻り値はActiveRecord::Relationオブジェクトなので、ScopeからScopeを呼び出すことも可能
