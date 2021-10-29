2ceccb53094e7f58b0f372b64bbbe51c9eae7354
sum16 <godasper@gmail.com>
2021-10-29 15:40:08 +0900

    station6/検索機能の修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,16 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    # @search_params = movie_search_params
-    @search_movies
+    #indexアクションが呼ばれると@moviesに値がはいってる
+    # @movies = Movie.search(movie_search_params) unless movie_search_params.blank?
+    # binding.pry
+    # @movies_all = Movie.all if @movies.empty?
+
+    # パラメータが空であればMovie.allを表示させる
+    if movie_search_params[:search].blank? && movie_search_params[:keyword].blank? && movie_search_params[:is_showing].blank?
+      @movies_all = Movie.all
+    else
+      @movies = Movie.search(movie_search_params)
+    end
   end
 
   def search_movie
@@ -17,3 +25,4 @@ end
 
 # fetchメソッド
 # params.fetch(:search, {})は、params[:search]が空の場合{}をparams[:search]が空でない場合、params[:search]を返す
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,10 +13,13 @@ class Movie < ApplicationRecord
     return if movie_search_params.blank?
 
     # パラメータを指定して検索を実行
-    name_like(movie_search_params).filter_is_showing(movie_search_params)
+    filter_is_showing(movie_search_params).name_like(movie_search_params)
   end
   # nameが存在する場合、nameをlike検索する
   scope :name_like, -> (movie_search_params) { where('name LIKE?', "%#{movie_search_params[:keyword]}%").or(where('description LIKE?', "%#{movie_search_params[:keyword]}%")) if movie_search_params[:keyword].present? }
   # is_showingが存在する場合、true or falseを返す
   scope :filter_is_showing, -> (movie_search_params) { where(is_showing: movie_search_params[:is_showing]) if movie_search_params[:is_showing].present? }
 end
+
+# where句は前方一致はindexが走るが部分一致はindexがはしらないため先にもってくる
+# よって先に公開フラグ判定のクエリを実行した方が良い
