3f66681361907b7e2f315db8f7452848068dd3ac
ryoichiro3816 <ryoichiro3816@gmail.com>
2021-11-29 14:41:20 +0900

    modify models/movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum:50 }
+  validates :year, length: { maximum:45 }
+  validates :image_url, length: { maximum:150 }
 end
