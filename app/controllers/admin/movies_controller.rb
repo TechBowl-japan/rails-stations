9e1d2d3ade86944041c7dcf87833fac3f34fc904
ryoichiro3816 <ryoichiro3816@gmail.com>
2021-11-29 14:24:40 +0900

    modify admin/movies_controller.rb

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -8,13 +8,20 @@ class Admin::MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(params[:movie])
+    @movie = Movie.new(movie_params)
     if @movie.save
       flash[:success] = "Welcome!"
-      redirect_to @movie
+      redirect_to '/movies'
     else
       flash.now[:danger] = "Error"
       render 'new'
     end
   end
+
+  private 
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
+
 end
