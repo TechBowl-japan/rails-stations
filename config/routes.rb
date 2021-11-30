8ceea99f9232cef4d96576b34f1e845a0233d7dc
ryoichiro3816 <ryoichiro3816@gmail.com>
2021-11-30 12:38:05 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/', to: 'movies#index'
-
   namespace :admin do
     get 'movies', to: 'movies#index'
     get 'movies/new', to:'movies#new'
@@ -9,5 +7,6 @@ Rails.application.routes.draw do
     patch 'movies/:id', to:'movies#update'
     put 'movies/:id', to:'movies#update'
   end
+  get '/', to: 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
