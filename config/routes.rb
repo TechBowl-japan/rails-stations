46259703b8169ce5be03d2f0bb839ccdeecc1813
ryoichiro3816 <ryoichiro3816@gmail.com>
2021-11-30 11:49:14 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get '/movies', to: 'movies#index'
+  get '/', to: 'movies#index'
 
   namespace :admin do
-    get '/movies', to: 'movies#index'
-    get '/movies/new', to:'movies#new'
-    post '/movies', to:'movies#create'
-    get '/movies/:id/edit', to:'movies#edit'
-    patch '/movies/:id', to:'movies#update'
-    put 'movies/:id', to:'movies#update'
+    resources :movies
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
