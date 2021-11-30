704b13c2df3cba30d7e1d7ef822d99199049c871
ryoichiro3816 <ryoichiro3816@gmail.com>
2021-11-30 11:41:30 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     post '/movies', to:'movies#create'
     get '/movies/:id/edit', to:'movies#edit'
     patch '/movies/:id', to:'movies#update'
+    put '/movies/:id', to:'movies#update'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
