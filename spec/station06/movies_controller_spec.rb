3729b4603801ecb54c6c66d17a49528fcbadc1e1
soheyHashimoto <heyso1209@gmail.com>
2021-12-22 18:45:20 +0900

    [CLEAN]Staion06のリファクタリング

--- spec/station06/movies_controller_spec.rb
+++ spec/station06/movies_controller_spec.rb
@@ -30,22 +30,26 @@ RSpec.describe MoviesController, type: :controller do
 
     context '映画作品リスト特有の仕様' do
       it 'method = getのformがある' do
-        expect(response.body).to include('method="get"')
+        expect(response.body).to include('method')
+        expect(response.body).to include('get').or include('GET')
       end
 
       it '検索キーワードを指定するとそれを含むものだけ表示' do
         selected_movie = create(:movie)
-        get :index, params: { name: selected_movie.name, is_showing: "" }
+        get :index, params: { name: selected_movie.name }
 
         expect(response.body).to include(selected_movie.name)
-        expect(response.body).to not_include(@movie[0].name)
-        expect(response.body).to not_include(@movie[1].name)
-        expect(response.body).to not_include(@movie[2].name)
+        expect(response.body).to not_include(@movies[0].name)
+        expect(response.body).to not_include(@movies[1].name)
+        expect(response.body).to not_include(@movies[2].name)
       end
 
       it '公開中か公開前の切り替えができる' do
-        get :index, params: { name: "", is_showing: 1 }
-        expect(response.body).to include("公開予定")
+        create(:movie, is_showing: true)
+        get :index, params: { is_showing: 1 }
+
+        expect(response.body).to include("公開中")
+        expect(response.body).to not_include("公開予定")
       end
     end
   end
