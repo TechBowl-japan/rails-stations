c34021e783b57d739da24b828f30bd0585d12225
soheyHashimoto <heyso1209@gmail.com>
2021-12-22 17:51:04 +0900

    [ADD]Station5のテスト修正

--- spec/station05/movies_controller_spec.rb
+++ spec/station05/movies_controller_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe Admin::MoviesController, type: :controller do
   render_views
   describe 'Station5 DELETE admin/movies/:id' do
-    let!(:movie) { create(:movie) }
+    before do
+      @movie = create(:movie)
+    end
+
     it 'リクエストを送ると320が返り、movies(:id)のレコードが消えること' do
       expect do
-        delete :destroy, params: { id: movie.id }, session: {}
+        delete :destroy, params: { id: @movie.id }, session: {}
       end.to change(Movie, :count).by(-1)
+
       expect(response).to have_http_status(302)
     end
-  
+
     it ':idのレコードが存在しないときRecordNotFound(400)が返る' do
-      nothing_movie_id = movie.id + 1
+      nothing_movie_id = @movie.id + 1
       expect do
         delete :destroy, params: { id: nothing_movie_id }, session: {}
       end.to raise_exception(ActiveRecord::RecordNotFound)
