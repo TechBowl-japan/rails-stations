270591bb3f1ab6ebae23eaff755babbe28e4c51e
soheyHashimoto <heyso1209@gmail.com>
2021-12-22 18:05:59 +0900

    [ADD]テストをまとめる

--- spec/station06/movies_controller_spec.rb
+++ spec/station06/movies_controller_spec.rb
@@ -4,21 +4,28 @@ RSpec::Matchers.define_negated_matcher :not_include, :include
 RSpec.describe MoviesController, type: :controller do
   render_views
   describe 'Station6 GET /movies' do
-    let!(:movies) { create_list(:movie, 3) }
     before do
+      @movies = create_list(:movie, 3)
       get 'index'
     end
 
-    it '200を返すこと' do
-      expect(response).to have_http_status(200)
-    end
+    context "Station01の仕様" do
+      it '200を返すこと' do
+        expect(response).to have_http_status(200)
+      end
 
-    it 'HTMLを返すこと' do
-      expect(response.body).to include('<!DOCTYPE html>')
-    end
-    
-    it 'HTMLの中にはmoviesテーブルのレコード数と同じ件数のデータがあること' do
-      expect(response.body).to include(movies[0].name).and include(movies[1].name).and include(movies[2].name).and include(movies[0].image_url).and include(movies[1].image_url).and include(movies[2].image_url)
+      it 'HTMLを返すこと' do
+        expect(response.body).to include('<!DOCTYPE html>')
+      end
+
+      it 'HTMLの中にはmoviesテーブルのレコード数と同じ件数のデータがあること' do
+        expect(response.body).to include(@movies[0].name)
+        expect(response.body).to include(@movies[1].name)
+        expect(response.body).to include(@movies[2].name)
+        expect(response.body).to include(@movies[0].image_url)
+        expect(response.body).to include(@movies[1].image_url)
+        expect(response.body).to include(@movies[2].image_url)
+      end
     end
 
     it 'method = getのformがある' do
@@ -29,7 +36,7 @@ RSpec.describe MoviesController, type: :controller do
       # factoriesのis_showingのデフォルトでは1
       let!(:show_estimated) { create(:movie, is_showing: 0 ) }
       let!(:showed_movie) { create(:movie, is_showing: 1 ) }
-  
+
       it '検索キーワードを指定するとそれを含むものだけ表示' do
         get :index, params: { name: show_estimated.name, is_showing: "" }
         expect(response.body).to include(show_estimated.name)
