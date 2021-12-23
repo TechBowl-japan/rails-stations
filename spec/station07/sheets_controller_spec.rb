c8349c6e0b9bfce68488bab2134725ffdff01657
soheyHashimoto <heyso1209@gmail.com>
2021-12-23 09:38:35 +0900

    [ADD]Station07のリファクタリング

--- spec/station07/sheets_controller_spec.rb
+++ spec/station07/sheets_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe SheetsController, type: :controller do
   render_views
   describe 'Station7 GET /sheets' do
-    let!(:sheets) { create_list(:sheet, 5) } 
     before do
-      get 'index'
+      @sheets = create_list(:sheet, 20)
+      get :index
     end
 
     it '200を返すこと' do
@@ -16,8 +16,9 @@ RSpec.describe SheetsController, type: :controller do
       expect(response.body).to include('</table>')
     end
 
-    it '実装の中でsheetsテーブルにアクセスしていること' do
-      expect(response.body).to include("#{sheets.first.column}").and include("#{sheets.last.column}")
+    it '実装の中でsheetsテーブルにアクセスして、マスを固定値で固めていないこと。' do
+      expect(response.body).to include("#{@sheets.first.column}")
+      expect(response.body).to include("#{@sheets.last.column}")
     end
   end
 end
