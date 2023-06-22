df595620a27dfc6d3a923dd29894a4fac476f0b0
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2023-06-23 01:09:01 +0900

    追加する内容は、Station12だったので、追加とrename

--- spec/station11/movies_controller_spec.rb
+++ spec/station11/movies_controller_spec.rb
@@ -2,33 +2,45 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
   render_views
-  describe 'Station11 GET /movies/:id' do
+  describe 'Station11 GET /movies/:id/reservation' do
+    let!(:sheets) { create_list(:sheet, 15) }
     let!(:movie) { create(:movie) }
-    let!(:schedule) { create_list(:schedule, 3, movie_id: movie.id) }
-    before do
-      get :show, params: { id: movie.id }, session: {}
+    let!(:schedule) { create(:schedule, movie_id: movie.id) }
+    let(:success_request) { get :reservation, params: { id: movie.id, movie_id: movie.id, schedule_id: schedule.id, date: "2021-12-21 14:53:56" }, session: {} }
+    let(:no_date_request) { get :reservation, params: { id: movie.id, movie_id: movie.id, schedule_id: schedule.id }, session: {} }
+    let(:no_schedule_request) { get :reservation, params: { id: movie.id, movie_id: movie.id, date: "2021-12-21 14:53:56" }, session: {} }
+    let(:no_date_and_sheet_request) { get :reservation, params: { id: movie.id, movie_id: movie.id }, session: {} }
+
+    context "クエリについて" do
+      it 'schedule_idとdateが渡されていれば200を返すこと' do
+        success_request
+        expect(response).to have_http_status(200)
+      end
+
+      it 'パラメーターにschedule_idがないときに302を返していること' do
+        no_schedule_request
+        expect(response).to have_http_status(302)
+      end
+
+      it 'パラメーターにdateがないときに302を返していること' do
+        no_date_request
+        expect(response).to have_http_status(302)
+      end
+
+      it 'パラメーターにdateとschedule_idがないときに302を返していること' do
+        no_date_and_sheet_request
+        expect(response).to have_http_status(302)
+      end
     end
 
-    context 'ページの仕様' do
-      it '1週間先まで選択可能な日付のプルダウンメニューが存在すること' do
-        for num in 0..6 do
-          expect(response.body).to include('<select').and include('value="'+Date.today.next_day(num).to_s()+'"')
-        end
-      end
+    it 'HTMLの中にはtableタグがあること' do
+      success_request
+      expect(response.body).to include('</table>')
+    end
 
-      it '上映スケジュールリストから1つの時刻を選択するプルダウンメニューが存在すること' do
-        for menu in schedule do
-          expect(response.body).to include('<option').and include(menu.id.to_s)
-        end
-      end
-
-      it '「座席を予約する」ボタンが存在すること' do
-        expect(response.body).to include('<button').and include('座席を予約する')
-      end
-
-      it '「座席を予約する」ボタンを押すと`/movies/:movie_id/reservation`に遷移すること' do
-        expect(response.body).to include('<form').and include('action="/movies/' + movie.id.to_s + '/reservation')
-      end
+    it '実装の中でsheetsテーブルにアクセスしていること' do
+      success_request
+      expect(assigns(:sheets)).to eq sheets
     end
   end
 end
---
+++ spec/station12/reservations_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe ReservationsController, type: :controller do
+  render_views
+  describe 'Station12 GET /movies/:movie_id/schedules/:schedule_id/reservations/new' do
+    let(:movie) { create(:movie) }
+    let(:sheets) { create_list(:sheet, 5) }
+    let(:date) { "2023-01-31" }
+    let(:schedule) { create(:schedule, movie_id: movie.id) }
+    let(:success_request) { get :new, params: { movie_id: movie.id, schedule_id: schedule.id, date: date, sheet_id: sheets.first.id }, session: {} }
+    let(:no_date_and_sheet_request) { get :new, params: { movie_id: movie.id, schedule_id: schedule.id }, session: {} }
+    let(:no_date_request) { get :new, params: { movie_id: movie.id, schedule_id: schedule.id, sheet_id: sheets.first.id }, session: {} }
+    let(:no_sheet_request) { get :new, params: { movie_id: movie.id, schedule_id: schedule.id, date: date }, session: {} }
+
+    context "クエリについて" do
+      it 'date, sheet_idの両方があるときだけ200を返すこと' do
+        success_request
+        expect(response).to have_http_status(200)
+      end
+
+      it 'date, sheet_idの両方が渡されていないとき200を返していないこと' do
+        no_date_and_sheet_request
+        expect(response).not_to have_http_status(200)
+      end
+
+      it 'dateが渡されていないとき200を返していないこと' do
+        no_date_request
+        expect(response).not_to have_http_status(200)
+      end
+
+      it 'sheet_idが渡されていないとき200を返していないこと' do
+        no_sheet_request
+        expect(response).not_to have_http_status(200)
+      end
+    end
+
+    it 'form送信時にmovie_id, schedule_id, sheet_id, name, emailのすべてを送信するようになっていること' do
+      success_request
+      expect(response.body).to include("name")
+      expect(response.body).to include("email")
+      expect(response.body).to include("schedule_id")
+      expect(response.body).to include("sheet_id")
+    end
+  end
+
+  describe 'Station12 POST /reservation/' do
+    let(:movie) { create(:movie) }
+    let(:sheets) { create_list(:sheet, 5) }
+    let(:date) { "2023-01-31" }
+    let(:schedule) { create(:schedule, movie_id: movie.id) }
+    let(:success_request) { post :create, params: { reservation: { name: "TEST_NAME", email: "test@test.com", date: date, sheet_id: sheets.first.id , schedule_id: schedule.id, movie_id: movie.id }}, session: {} }
+
+    it 'schedule_id, sheet_id, name, email, dateのすべてがあるときに302を返す' do
+      success_request
+      expect(response).to have_http_status(302)
+    end
+
+    it 'DBのunique制約にかかったときにリダイレクトテスト' do
+      # 同じ日付の同じ映画の座席を予約してみる
+      create(:reservation, { sheet_id: sheets.first.id, schedule_id: schedule.id, date: date })
+      success_request
+      # NOTE: movie_reservation_pathとして指定したいが、実装の揺れに対応するために文字列で指定している
+      expect(response).to redirect_to("http://test.host/movies/#{movie.id}/reservation?schedule_id=#{schedule.id}&date=#{date}").or redirect_to("http://test.host/movies/#{movie.id}/reservation?date=#{date}&schedule_id=#{schedule.id}")
+    end
+  end
+end
