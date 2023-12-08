e360fb7b51c7dcd890f9ea3fe9dccb8827c01602
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2023-12-08 23:11:21 +0900

    turbolinksから脱却する

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+    },
+  },
+};
+
+environment.plugins.delete("Compression");
+environment.plugins.delete("Compression Brotli");
+environment.config.delete("node.dgram");
+environment.config.delete("node.fs");
+environment.config.delete("node.net");
+environment.config.delete("node.tls");
+environment.config.delete("node.child_process");
+environment.config.merge(customConfig);
+
+module.exports = environment;
