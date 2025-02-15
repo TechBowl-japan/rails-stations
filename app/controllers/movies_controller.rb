8c169004089aadcf48184f5072b00e96d8a802d6
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-15 21:46:00 +0900

    station6｜controller｜検索ワードがある場合にフィルタリングを適用

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,31 @@
 class MoviesController < ApplicationController
     def index 
       @movies = Movie.all
+      @movies = filter_by_showing_status(@movies)
+      @movies = filter_by_keyword(@movies)
     end
+
+    # 上映フィルタリング
+    def filter_by_showing_status(movies)
+      case params[:is_showing]
+      when "1"
+        movies.where(is_showing: true)
+      when "0"
+        movies.where(is_showing: false)
+      else
+        movies
+      end
+    end
+
+    # タイトルフィルタリング
+    def filter_by_keyword(movies)
+      if params[:keyword].present?
+        movies.where("name LIKE ? OR description LIKE ?",
+          "%#{params[:keyword]}%",
+          "%#{params[:keyword]}%")
+      else
+        movies
+      end
+    end
+
 end
