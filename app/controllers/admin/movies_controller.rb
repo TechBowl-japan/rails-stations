4187f7cb51a3adb38a0427274be44bac4958d8f4
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-15 16:31:03 +0900

    station3｜admin｜controllerにnew,createアクションを追加

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -3,6 +3,26 @@ class Admin::MoviesController < ApplicationController
     def index 
       @movies = Movie.all
     end
+
+    def new
+      @movie = Movie.new
+    end
+
+    def create
+      @movie = Movie.new(movie_params)
+      if @movie.save
+        redirect_to admin_movies_path, notice: "映画が登録されました"
+      else
+        flash.now[:alert] = "登録に失敗しました"
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def movie_params
+      params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    end
 end
 
 # Admin:: は名前空間を表す、:: は名前空間の区切り文字
