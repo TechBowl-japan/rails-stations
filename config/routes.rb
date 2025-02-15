661bb2f043b609a9ed5bf5dfdd4c9680f433552a
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-15 15:40:54 +0900

    station3｜admin｜rootingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :movies, only: [:index]
 
   namespace :admin do
-    resources :movies, only: [:index]
+    resources :movies, only: [:index, :new, :create]
   end
   # Defines the root path route ("/")
   # root "posts#index"
