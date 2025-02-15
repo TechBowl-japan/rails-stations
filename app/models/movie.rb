e42e2dac5c9ce61f5775800944d5416784e21556
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-15 21:47:21 +0900

    station6｜controller｜ActiveRecordの処理

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,13 @@ class Movie < ApplicationRecord
     validates :description, presence: true
     validates :image_url, presence: true
     validates :is_showing, presence: true
+
+    # フィルタリング
+    scope :showing, -> { where(is_showing: true)}
+    scope :not_showing, -> { where(is_showing: false)}
+    scope :search_by_keyword, -> (keyword) {
+      where("name LIKE ? OR description LIKE ?",
+        "%#{keyword}%",
+        "%#{keyword}%")
+    }
 end
