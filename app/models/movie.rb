231c3233b903efb6528b796c90e9bb2f8f0038a9
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-15 16:34:21 +0900

    station3｜admin｜映画名の重複を禁止するvalidationを追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
     # バリデーション presence: true → 各フィールドが必須（空を許可しない）
-    validates :name, presence: true
+    # uniqueness: true → 一意でなければならない
+    validates :name, presence: true, uniqueness: true
     validates :year, presence: true
     validates :description, presence: true
     validates :image_url, presence: true
