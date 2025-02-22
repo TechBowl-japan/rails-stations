06d487797eb5df5e73d71a8524597de8875665f1
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-22 19:04:08 +0900

    station11｜models｜Sheetモデルの関連付け追加

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,4 +1,5 @@
 class Sheet < ApplicationRecord
+    has_many :reservations
     validates :column, presence: true, numericality: { only_integer: true }
     validates :row, presence: true, length: { is: 1 }
 end
