eac41bc39f34e9f78e4e6f106863e9b1b82c8774
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-24 20:39:46 +0900

    station13｜models｜関連モデルの修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,8 @@
 class Schedule < ApplicationRecord
+  belongs_to :screen
   belongs_to :movie
+  has_many :reservations
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validates :screen_id, presence: true
 end
--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,5 +1,7 @@
 class Sheet < ApplicationRecord
+    belongs_to :screen
     has_many :reservations
     validates :column, presence: true, numericality: { only_integer: true }
     validates :row, presence: true, length: { is: 1 }
+    validates :screen_id, presence: true
 end
