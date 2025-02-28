444f6510309e696a145e422268fa2998fa0a49c1
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-28 20:47:35 +0900

    station14｜テストコードエラー対応
    1. 名前フィールドが必須に
    2. パラメータが正しく許可される
    3. バリデーションエラーが適切に表示される
    4. フォームの必須チェックが機能する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reservations
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, if: :password_required?
+  validates :password_confirmation, presence: true, if: :password_required?
 end
