1ab5da49d2ba18d046f17d142c1aa64d9fa92c0e
kamioka24 <keee2404@gmail.com>
2026-06-01 17:23:04 +0900

    Fix reservation_spec.rb

--- spec/station12/reservation_spec.rb
+++ spec/station12/reservation_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  describe 'Station12 Are date&schedule_id&sheet_id unique?' do
+  describe 'Station12 Are date&schedule_id&seat_id unique?' do
     let(:movie) { create(:movie) }
-    let(:sheet) { create(:sheet) }
+    let(:seat) { create(:seat) }
     let(:schedule) { create(:schedule, movie_id: movie.id) }
-    let(:reservation) { create(:reservation, { sheet_id: sheet.id, schedule_id: schedule.id }) }
-    let(:duplicated_reservation) { build(:reservation, { sheet_id: sheet.id, schedule_id: schedule.id, date: reservation.date }) }
+    let(:reservation) { create(:reservation, { seat_id: seat.id, schedule_id: schedule.id }) }
+    let(:duplicated_reservation) { build(:reservation, { seat_id: seat.id, schedule_id: schedule.id, date: reservation.date }) }
 
     it "変更することで他の予約と全く同じような予約になること" do
       expect(duplicated_reservation).not_to be_valid
