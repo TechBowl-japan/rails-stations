2f8cf5f086fa39beef94b672286bfef0ec0438ea
kamioka24 <keee2404@gmail.com>
2026-06-01 17:24:04 +0900

    Fix reservations_controller_spec.rb

--- spec/station13/reservations_controller_spec.rb
+++ spec/station13/reservations_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ReservationsController, type: :controller do
   describe 'Station13 GET /movies/:movie_id/schedules/:schedule_id/reservations/new' do
     let(:failure_request) { get :new, params: { movie_id: 1, schedule_id: 1 }, session: {} }
 
-    it 'date, sheet_idのどちらかまたは両方が渡されていないとき200を返していないこと' do
+    it 'date, seat_idのどちらかまたは両方が渡されていないとき200を返していないこと' do
       failure_request
       expect(response).not_to have_http_status(200)
     end
