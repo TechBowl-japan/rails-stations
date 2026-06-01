a7ef6f1bad5009122fdf523556271dfcb278c001
kamioka24 <keee2404@gmail.com>
2026-06-01 17:25:44 +0900

    Fix reservation.rb

--- spec/factories/reservation.rb
+++ spec/factories/reservation.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :reservation do
     association :schedule, factory: :schedule
-    association :sheet, factory: :sheet
+    association :seat, factory: :seat
     sequence(:name) { |n| "TEST_USER#{n}" }
     sequence(:email) { |n| "test_email#{n}@test.com" }
     sequence(:date) { |n| "2019-04-1#{n}"}
